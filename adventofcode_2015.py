14b410676f0d5009d23fed27564fbca24e93e4b9
David Xue <David.Xue37@myhunter.cuny.edu>
2022-08-18 17:04:21 -0400

    Day 7

--- adventofcode_2015.py
+++ adventofcode_2015.py
@@ -228,6 +228,36 @@ def adventofcode_2015_six_part2(file):
             total_bright += j
     return total_bright 
 
+def adventofcode_2015_seven_part1_and_2(file):
+    our_var = {}
+    while('a' not in our_var):
+        with open(file) as f:
+            for line in f:
+                line = line.replace('-> ','').strip('\n').split(' ')
+                if(len(line)==2 and line[0].isnumeric()):
+                    our_var[line[1]] = int(line[0]) 
+                elif(len(line)==2 and not line[0].isnumeric() and line[0] in our_var):
+                    our_var[line[1]] = int(our_var[line[0]])
+                elif(len(line)==3 and not line[0].isnumeric() and line[1] in our_var):
+                    our_var[line[2]] = 65535 - int(our_var[line[1]])
+                elif(len(line)==4 and line[1]=='RSHIFT' and line[2].isnumeric() and line[0] in our_var):
+                    our_var[line[3]] = our_var[line[0]] >> int(line[2])
+                elif(len(line)==4 and line[1]=='LSHIFT' and line[2].isnumeric() and line[0] in our_var):
+                    our_var[line[3]] = our_var[line[0]] << int(line[2])
+                elif(len(line)==4 and line[1]=='AND' and line[0].isnumeric() and line[2] in our_var):
+                    our_var[line[3]] = int(line[0]) & our_var[line[2]]
+                elif(len(line)==4 and line[1]=='AND' and line[2].isnumeric() and line[0] in our_var):
+                    our_var[line[3]] = int(line[2]) & our_var[line[0]]
+                elif(len(line)==4 and line[1]=='AND' and line[0] in our_var and line[2] in our_var):
+                    our_var[line[3]] = our_var[line[0]] & our_var[line[2]]
+                elif(len(line)==4 and line[1]=='OR' and line[0].isnumeric() and line[2] in our_var):
+                    our_var[line[3]] = int(line[0]) | our_var[line[2]]
+                elif(len(line)==4 and line[1]=='OR' and line[2].isnumeric() and line[0] in our_var):
+                    our_var[line[3]] = int(line[2]) | our_var[line[0]]
+                elif(len(line)==4 and line[1]=='OR' and line[0] in our_var and line[2] in our_var):
+                    our_var[line[3]] = our_var[line[0]] | our_var[line[2]]
+    return our_var['a']
+
 #2015
 #---------------------------------------------
 
@@ -255,3 +285,6 @@ def adventofcode_2015_six_part2(file):
 #print(adventofcode_2015_six_part2('text_inputs/adventofcode_2015_6.txt'))
 
 #Day 7 
+#print(adventofcode_2015_seven_part1_and_2('text_inputs/adventofcode_2015_7.txt'))
+
+#Day 8 
