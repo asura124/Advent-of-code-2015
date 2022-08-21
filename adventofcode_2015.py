697218145a2383ab8ad964304bb436844ef100a5
David Xue <David.Xue37@myhunter.cuny.edu>
2022-08-21 16:10:44 -0400

    Day 9

--- adventofcode_2015.py
+++ adventofcode_2015.py
@@ -1,4 +1,5 @@
 import hashlib
+import itertools
 
 def adventofcode_2015_one_part1(file):
     floor = 0
@@ -302,6 +303,33 @@ def adventofcode_2015_eight_part2(file):
                     new_encode +=2
             new_encode += 6 #from the stripped "" 
     return new_encode - str_literal 
+
+def adventofcode_2015_nine_part1_and_part2(file):
+    location_1 =[]
+    location_2 =[]
+    distance = []
+    starting_unique_locations = [] 
+    with open(file) as f:
+        for line in f:
+            line = line.strip('\n').replace('to ','').replace('= ','').split(' ')
+            if(line[0] not in starting_unique_locations):
+                starting_unique_locations.append(line[0])
+            if(line[1] not in starting_unique_locations):
+                starting_unique_locations.append(line[1])
+            location_1.append(line[0])
+            location_2.append(line[1])
+            distance.append(int(line[2]))
+    comb = list(itertools.permutations(starting_unique_locations))
+    total_distances = []
+    for i in comb:
+        curr_dis = 0
+        for city in range(len(i)-1):
+            for k in range(len(location_1)):
+                if((i[city]==location_1[k] and i[city+1]==location_2[k]) or (i[city]==location_2[k] and i[city+1]==location_1[k])):
+                    curr_dis += distance[k]
+        total_distances.append(curr_dis)
+    return min(total_distances) #return max(total_distances) for part 2
+
 #2015
 #---------------------------------------------
 
@@ -336,3 +364,6 @@ def adventofcode_2015_eight_part2(file):
 #print(adventofcode_2015_eight_part2('text_inputs/adventofcode_2015_8.txt'))
 
 #Day 9 
+#print(adventofcode_2015_nine_part1_and_part2('text_inputs/adventofcode_2015_9.txt'))
+
+#Day 10 
