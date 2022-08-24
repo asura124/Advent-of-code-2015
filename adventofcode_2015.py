e9575e5e06cca11d56dc2e6b88744900a688c869
David Xue <David.Xue37@myhunter.cuny.edu>
2022-08-24 13:50:48 -0400

    Day 11

--- adventofcode_2015.py
+++ adventofcode_2015.py
@@ -345,6 +345,47 @@ def adventofcode_2015_ten_part1_and_part2(code):
         code = temp
         #print(temp) 
     return len(code)
+
+def adventofcode_2015_eleven(password):
+    ascii_password = []
+    bad_letters_ascii = [105,108,111] #i,l,o
+    for i in password:
+        ascii_password.append(ord(i))
+    good_pass = False
+    ascii_password = password_increment(ascii_password)
+    while( not good_pass):
+        straight_increase = False
+        contains_bad_letters = False
+        pairs = 0
+        pair_index = [] 
+        for i in range(len(ascii_password)):
+            if(ascii_password[i] in bad_letters_ascii):
+                contains_bad_letters = True
+            if(i<len(ascii_password)-2):
+                if(ascii_password[i+1]==ascii_password[i]+1 and (ascii_password[i+2]==ascii_password[i+1]+1)):
+                    straight_increase = True
+            if(i<len(ascii_password)-1):
+                if(ascii_password[i]==ascii_password[i+1] and (i not in pair_index) and ((i+1) not in pair_index)):
+                    pairs +=1
+                    pair_index.append(i)
+                    pair_index.append(i+1)
+        if(pairs>=2 and not contains_bad_letters and straight_increase):
+            good_pass = True
+        else:
+            ascii_password = password_increment(ascii_password)
+    new_pass = ""
+    for i in ascii_password:
+        new_pass += chr(i)
+    return new_pass
+
+def password_increment(ascii_password):
+    last = -1
+    ascii_password[last] += 1
+    while(123 in ascii_password):
+        ascii_password[last] -= 26
+        last -= 1
+        ascii_password[last] +=1
+    return ascii_password
 #2015
 #---------------------------------------------
 
@@ -383,4 +424,8 @@ def adventofcode_2015_ten_part1_and_part2(code):
 
 #Day 10 
 #print(adventofcode_2015_ten('1'))
-print(adventofcode_2015_ten_part1_and_part2('1321131112'))
+#print(adventofcode_2015_ten_part1_and_part2('1321131112'))
+
+#Day 11
+print(adventofcode_2015_eleven('hepxcrrq'))
+print(adventofcode_2015_eleven('hepxxyzz'))
