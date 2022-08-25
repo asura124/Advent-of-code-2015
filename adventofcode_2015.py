c20925ce69d59927faf32fa1b17b722bf1869882
David Xue <David.Xue37@myhunter.cuny.edu>
2022-08-25 15:49:56 -0400

    Day 12 progress

--- adventofcode_2015.py
+++ adventofcode_2015.py
@@ -402,32 +402,43 @@ def adventofcode_2015_twelve_part1(file):
     return 0 
 
 def adventofcode_2015_twelve_part2(file):
-    object_open_backet = 0
+    bracket_arr = []
+    brack_sum = 0
     total = 0
-    curr_num = 0 
+    curly_sum = 0 
     ignore = False
     with open(file) as f:
         for line in f:
             line = line.replace('"','').replace('[','[,').replace(']',',]').replace(':',',').replace('{','{,').replace('}',',}').strip('\n')
             line = line.split(',')
             for l in line:
+                l = l.strip()
                 print(l)
                 if(l=='{'):
-                    object_open_backet += 1
-                elif(object_open_backet>0 and l=='red'):
-                    ignore = True
-                elif(l.isnumeric() and object_open_backet>0):
-                    curr_num += int(l)
-                elif(l.isnumeric() and object_open_backet==0):
-                    total += int(l)
+                    bracket_arr.append('{')
+                elif(l=='['):
+                    bracket_arr.append('[')
                 elif(l=='}'):
                     if(ignore):
                         total += 0
                     else:
-                        total += curr_num
-                    curr_num = 0
-                    object_open_backet -=1
+                        total = total + curly_sum + brack_sum
+                    curly_sum = 0
+                    brack_sum = 0 
+                    bracket_arr.pop()
                     ignore = False
+                elif(l==']'):
+                    bracket_arr.pop()
+                elif(len(bracket_arr)>0):
+                    if(bracket_arr[-1]=='{' and l=='red'):
+                        ignore = True
+                    elif(l.strip('-').isnumeric() and bracket_arr[-1]=='{'):
+                        curly_sum += int(l)
+                    elif(l.strip('-').isnumeric() and bracket_arr[-1]=='['):
+                        brack_sum += int(l)
+                elif(l.isnumeric() and len(bracket_arr)==0):
+                    total += int(l)
+                #print(l,curly_sum,bracket_arr,total)
     return total
 #2015
 #---------------------------------------------
